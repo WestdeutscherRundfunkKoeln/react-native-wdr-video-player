070e91fff0ac3cee7199a901fc63252cf9634d82
Dirk Kerpen <dirk.kerpen@wdr.de>
2019-10-01 08:58:20 +0200

    Slider-Component local installiert und geändert

--- components/Scrubber.js
+++ components/Scrubber.js
@@ -6,7 +6,7 @@ import {
   StyleSheet,
   Slider as RNSlider
 } from 'react-native'
-import Slider from 'react-native-slider'
+import Slider from './Slider'
 
 const styles = StyleSheet.create({
   container: {
